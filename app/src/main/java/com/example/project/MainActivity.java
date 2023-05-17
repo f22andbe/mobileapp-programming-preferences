016898bff874f233c8296dce55933b0a45c1a424
f22andbe <f22andbe@student.his.se>
2023-05-17 22:57:24 +0200

    Setup activities

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences settings;
+    Button switchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        switchActivity = findViewById(R.id.switch_acitivity_button);
+
+        settings = getSharedPreferences("shared_prefs", MODE_PRIVATE);
+
+    }
+
+    public void onClickListener(View v) {
+        startActivity(new Intent(this, EditPreferenceActivity.class));
+    }
+
+    protected void onResume() {
+        super.onResume();
     }
 
 }
